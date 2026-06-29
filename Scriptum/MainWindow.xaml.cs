963acf6d7a8d077eef536e04386804be4e153d3c
051-lab <darius.vinson@tech901.org>
2026-06-29 07:00:32 -0700

    chore: simplify main window initialization

--- Scriptum/MainWindow.xaml.cs
+++ Scriptum/MainWindow.xaml.cs
@@ -7,8 +7,6 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
-        // Set the content to MainView for MVVM navigation
         Content = new Views.MainView();
     }
 }
