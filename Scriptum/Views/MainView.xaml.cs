fc6d1e046e34668ccce7b1d32f936c621185ac10
051-lab <darius.vinson@tech901.org>
2026-06-29 07:00:50 -0700

    chore: initialize main view model consistently

--- Scriptum/Views/MainView.xaml.cs
+++ Scriptum/Views/MainView.xaml.cs
@@ -1,18 +1,17 @@
-using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Scriptum.ViewModels;
 
 namespace Scriptum.Views;
 
 /// <summary>
-/// Code-behind for MainView.
+/// Code-behind for the main application view.
 /// </summary>
 public sealed partial class MainView : Page
 {
-    public MainViewModel ViewModel { get; }
+    public MainViewModel ViewModel { get; } = new();
 
     public MainView()
     {
-        ViewModel = new MainViewModel();
         InitializeComponent();
         DataContext = ViewModel;
     }
